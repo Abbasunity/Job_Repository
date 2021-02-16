6f79a89537911b7d35bf0977f928383634de9b7e
Abbasunity <76579952+Abbasunity@users.noreply.github.com>
2021-02-16 10:05:14 +0530

    d
    d

--- Job_dsl.groovy
+++ Job_dsl.groovy
@@ -1,23 +1,25 @@
-job{'NodeJS example'} {
 
-scm {
-	
+job('NodeJS example') {
+
+	scm {
 		git('https://github.com/Abbasunity/FormValidationJankins') { node ->
 			node / gitConfigName('DSL User')
 			node / gitConfigEmail('abbas14uit@gmail.com')
 			
-			}
 		}
+	}
 	triggers {
-	SCM('H/5 * * * *')
+		SCM('H/5 * * * *')
+		
 	}
 	
 	wrappers {
 			
-			nodejs('nodejs')
+		nodejs('nodejs')
 	}
 	
 	steps{
 		shell("npm install")
-		}
+		
+	}
 }
